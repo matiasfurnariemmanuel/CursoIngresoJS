20bc1e1deff8b0c9d6f8979cd697ebb7f5ed1f4b
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-01-30 14:57:36 -0300

    Nombre

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,12 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	alert("ok");
+function mostrar() {
+	var nombre;
+	var nombre = prompt("Escriba un nombre")
+
+	alert(nombre);
+
+
 }
 
