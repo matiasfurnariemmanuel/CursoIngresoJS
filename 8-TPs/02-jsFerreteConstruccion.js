8d65125bed0ff62a471bba3d49437e2e204ee147
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-02-01 16:46:01 -0300

    terminado

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,18 +1,40 @@
-/*2.	Para el departamento de Construcción:
+/*2.Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
-
+function Rectangulo() {
+    let ancho;
+    let largo;
+    let perimetro;
+    let alambre;
+    largo = parseInt(document.getElementById("txtIdLargo").value);
+    ancho = parseInt(document.getElementById("txtIdAncho").value);
+    perimetro = (largo + ancho) * 2;
+    alambre = perimetro * 3;
+    alert = (`Se necesitan comprar ${alambre} metros de alambre`)
 }
-function Circulo () 
-{
-	
+function Circulo() {
+    let perimetro;
+    let radio;
+    let alambre;
+    const PI = Math.PI;//la constante se escribe en mayuscula y se le asigna el valor en la misma linea donde se declara.
+    radio = parseInt(document.elementFromPoint("txtIdRadio").value);
+    perimetro = 2 * PI * radio;
+    alambre = perimetro * 3;
+    alert = (`Se necesitan comprar ${alambre} metros de alambre`);
 }
-function Materiales () 
-{
-	
+function Materiales() {
+    let ancho;
+    let largo;
+    let area;
+    let cemento;
+    let cal;
+    largo = parseInt(document.getElementById("txtIdLargo").value);
+    ancho = parseInt(document.getElementById("txtIdAncho").value);
+    area = largo * ancho;
+    cemento = area * 2;
+    cal = area * 3;
+    alert("Para un contrapiso de " + area + "m2 necesito comprar " + cemento + " bolsas de cemento y " + cal + " bolsas de cal");
 }
