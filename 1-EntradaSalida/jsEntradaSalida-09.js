be556620890e23d8f7b632705301148a181a33a8
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-01-31 23:12:23 -0300

    sueldo%

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,17 @@
 /*
-Debemos lograr tomar el importe por ID ,
+Debemos lograr tomar el sueldo por ID ,
 transformarlo a entero (parseInt), luego
-mostrar el importe con un aumento del 10 %
+mostrar el nuevo suedo con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+   let sueldo;
+   let nuevosueldo;
+   let aumento;
+   sueldo = parseFloat(document.getElementById("txtIdSueldo").value);
+   aumento = (sueldo * 10)/100;
+   nuevosueldo = sueldo + aumento;
+   document.getElementById("txtIdResultado").value = nuevosueldo;
 
-	alert("ok");
+
 }
