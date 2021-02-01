2130f51c996812c9919c611c819dc385896933ec
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-02-01 00:19:52 -0300

    tp1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,41 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+function Sumar() {
+    let producto1;
+    let producto2;
+    let producto3;
+    let suma;
+    producto1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    producto2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    producto3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    suma = producto1 + producto2 + producto3;
+    alert(`La suma de los precios de los productos es: ${suma}`);
+
+
 }
-function Promedio () 
-{
-	
+function Promedio() {
+    let producto1;
+    let producto2;
+    let producto3;
+    let promedio;
+    producto1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    producto2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    producto3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    promedio =  (producto1 + producto2 + producto3)/3;
+    alert(`El promedio de los precios de los productos es: ${promedio}`);
+
 }
-function PrecioFinal () 
-{
-	
+function PrecioFinal() {
+    let producto1;
+    let producto2;
+    let producto3;
+    let preciofinal;
+    let iva;
+    producto1 = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    producto2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    producto3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    iva = ((producto1 + producto2 + producto3)*21)/100;
+    preciofinal = producto1 + producto2 + producto3 + iva;
+    alert(`El precio final de los productos con iva es: ${preciofinal}`);
 }
