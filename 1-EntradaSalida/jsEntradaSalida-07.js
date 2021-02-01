19d53ff819d2672b42c88e5ca780199a479ace27
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-01-31 21:35:00 -0300

    operaciones

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -2,24 +2,51 @@
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
-function sumar()
-{	
-	alert("ok");	
+ej.: "la Resta es 750"*/
+function sumar() {
+	let num1;
+	let num2;
+	let suma;
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	suma = num1 + num2;
+	alert(`La suma es: ${suma}`);
+
 }
 
-function restar()
-{
-	
+function restar() {
+	let num1;
+	let num2;
+	let resta;
+	num1 = parseFloat(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseFloat(document.getElementById("txtIdNumeroDos").value);
+	resta = num1 - num2;
+	alert(`La suma es: ${resta}`);
+
+
 }
 
-function multiplicar()
-{ 
-	
+function multiplicar() {
+	let num1;
+	let num2;
+	let multiplicar;
+	num1 = parseFloat(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseFloat(document.getElementById("txtIdNumeroDos").value);
+	multiplicar = num1 * num2;
+	alert(`La suma es: ${multiplicar}`);
+
+
 }
 
-function dividir()
-{
-	
+function dividir() {
+	let num1;
+	let num2;
+	let division;
+	num1 = parseFloat(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseFloat(document.getElementById("txtIdNumeroDos").value);
+	division = num1 / num2;
+	alert(`La suma es: ${division}`);
+
+
 }
 
