613ab86d92a9a0376a4ab4cd4b6b520cfff0da21
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-01-31 21:18:32 -0300

    suma

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -6,9 +6,11 @@ function sumar() {
 	let num1;
 	num1 = document.getElementById("txtIdNumeroUno").value;
 	num1 = parseInt(num1);
+	//num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
 	let num2;
 	num2 = document.getElementById("txtIdNumeroDos").value;
 	num2 = parseInt(num2);
+	//num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
 	let suma;
 	suma = num1 + num2;
 	//alert("el resultado es: " + suma);
