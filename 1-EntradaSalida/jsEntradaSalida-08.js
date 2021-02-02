6b63766536c25021b9c53e3dec79d14ad9b136fe
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-02-02 20:50:09 -0300

    RESTO

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,15 +1,16 @@
-/*
+/*FURNARI MATIAS "DIVISION H"
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
-function SacarResto()
-{
+function SacarResto() {
 	let num1;//dividendo
 	let num2;//divisor
 	let resto;
-	num1 = parseInt(document.getElementById("txtIdNumeroDividendo").value);
-	num2 = parseInt(document.getElementById("txtIdNumeroDivisor").value);
+	num1 = document.getElementById("txtIdNumeroDividendo").value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById("txtIdNumeroDividendo").value;
+	num2 = parseInt(num2);
 	resto = num1 % num2;
 	alert(`El resto es: ${resto}`);
 }
