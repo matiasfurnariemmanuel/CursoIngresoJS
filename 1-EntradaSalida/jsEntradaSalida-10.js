b37d2bb6affd35ab8508ddf175e3939c254bcc75
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-02-02 22:23:16 -0300

    DESCUENTO

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,4 +1,4 @@
-/*
+/*ALUMNO FURNARI MATIAS "DIVISION H"
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
@@ -10,7 +10,9 @@ function mostrarAumento() {
 	importe = document.getElementById("txtIdImporte").value;
 	importe = parseInt(importe);
 	descuento = importe * 25 / 100;
-	descuento = parseInt(descuento);
+	//descuento = importe*0.25;
+	//descuento = importe - (importe*0.75);
+	//importefinal= importe*0.75;
 	importefinal = importe - descuento;
 	document.getElementById("txtIdResultado").value = importefinal;
 }
