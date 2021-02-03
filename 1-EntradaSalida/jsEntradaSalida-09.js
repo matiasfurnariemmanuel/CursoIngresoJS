9dc77d68585298707fe4787fdf553bb0a69dd11f
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-02-02 21:53:15 -0300

    AUMENTO

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -3,15 +3,16 @@ Debemos lograr tomar el sueldo por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el nuevo suedo con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
-function mostrarAumento()
-{
+function mostrarAumento() {
    let sueldo;
    let aumento;
    let nuevosueldo;
    sueldo = document.getElementById("txtIdSueldo").value;
    sueldo = parseInt(sueldo);
-   aumento = (sueldo * 10)/100;
-   aumento = parseInt(aumento);
-   nuevosueldo= sueldo + aumento;
+   aumento = (sueldo * 10) / 100;
+   //aumento = sueldo * 0.1;
+   //aumento = sueldo *10 /100;
+   //resultado = sueldo + (sueldo*0.1);
+   nuevosueldo = sueldo + aumento;
    document.getElementById("txtIdResultado").value = nuevosueldo;
 }
