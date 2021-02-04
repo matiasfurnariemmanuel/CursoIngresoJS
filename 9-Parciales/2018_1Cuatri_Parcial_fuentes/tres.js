f8ac5b5be1b8ad24c382d7fb6be5d196eb80902a
matias furnari emmanuel <matiasfurnariemmanuel@gmail.com>
2021-02-04 19:46:22 -0300

    hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,20 @@
+/*
+Pedir por prompt el precio y el porcentaje de descuento,
+mostrar el precio final con descuento por id.
+*/
 function mostrar()
 {
+let precio;
+let porcentaje;
+let descuento;
+let precioFinal;
+precio = prompt("Ingrese el precio: ");
+precio = parseFloat(precio);
+porcentaje = prompt("Ingrese el porcentaje de descuento: ");
+porcentaje = parseFloat(porcentaje);
+descuento = precio*porcentaje/100;
+precioFinal = precio - descuento;
+document.getElementById("elPrecioFinal").value = precioFinal;
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
-
+/*Realizar el algoritmo que  pida el ancho y el largo 
+de un rectángulo por prompt y que muestre el perímetro por alert.
+*/
 function mostrar()
 {
+let ancho;
+let largo;
+let perimetro;
+ancho = prompt("Indique el ancho del rectangulo: ");
+ancho = parseFloat(ancho);
+largo = prompt("Indique el largo del rectangulo: ");
+largo = parseFloat(largo);
+perimetro = (ancho + largo)*2;
+alert("el perimetro del rectangulo es: "+perimetro);
+
 
 }
